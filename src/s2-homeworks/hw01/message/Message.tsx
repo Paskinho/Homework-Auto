b4c1c762db3b3480906c545ab1ce714323287364
Paskinho <v.paskanov@gmail.com>
2022-10-13 20:13:32 +0300

    change package.json

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import s from './Message.module.css'
+import {MessageType} from "../HW1";
 
-// нужно создать правильный тип вместо any
-export type MessagePropsType = any
+// нужно создать правильный тип вместо  any
+export type MessagePropsType = {
+    message: MessageType
+}
+
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
