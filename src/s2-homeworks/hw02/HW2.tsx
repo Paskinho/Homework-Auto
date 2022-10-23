5d50aa2219dc0d61abd17995a2a5f2aa07c3b711
Paskinho <v.paskanov@gmail.com>
2022-10-23 19:17:13 +0300

    Исправление ошибки в filterAffairs

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -8,7 +8,7 @@ import s2 from '../../s1-main/App.module.css'
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами +
 * 4 - выполнить пункт 3 для функции deleteAffair +
 * 5 - указать нужный тип в useState с affairs +
-* 6 - дописать тип и логику функции deleteAffairCallback
+* 6 - дописать тип и логику функции deleteAffairCallback +
 * 7 - в файле Affairs.tsx дописать типизацию пропсов
 * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
 * 9 - в файле Affair.tsx дописать типизацию пропсов
@@ -45,7 +45,7 @@ else if (filter === "high") {
 else if (filter === "middle") {
     return affairs.filter(p => p.priority === "middle")
 }
-else if (filter === "low") {
+else {
     return affairs.filter(p => p.priority === "low")
 }
 
