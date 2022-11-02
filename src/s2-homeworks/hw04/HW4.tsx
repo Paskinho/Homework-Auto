481d55b351d704116e790a5f061b4aa7f46becf4
Paskinho <v.paskanov@gmail.com>
2022-11-02 17:12:20 +0300

    Start HW4

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -7,8 +7,7 @@ import Stand from './Stand'
 * 2 - в зависимости от типа и дизэйбла прицепить нужный класс в SuperButton.tsx (строка 21)
 * 3 - дописать onChangeCallback в SuperCheckbox.tsx чтоб оба чекбокса работали на стенде
 * 4 - сделать стили в соответствии с дизайном
-* *
-/
+* */
 
 const HW4 = () => {
     return (
