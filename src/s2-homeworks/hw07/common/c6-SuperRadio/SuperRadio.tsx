ad756efa4fb460c37e5089d57d02c0caf68e378a
Paskinho <v.paskanov@gmail.com>
2022-12-03 15:05:11 +0300

    в файле SuperRadio.tsx дописать логику функции onChangeCallback +

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,6 +35,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
         // делают студенты
     }
 
