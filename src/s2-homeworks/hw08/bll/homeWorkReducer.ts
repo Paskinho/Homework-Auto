e443a9a481ffdc01cbf1547ea862fb5435a43891
Paskinho <v.paskanov@gmail.com>
2022-12-04 14:42:44 +0300

    8 HW sort by name

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,17 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+const newState = [...state].sort((a,b)=> {
+    if (a.name > b.name) return 1
+    else if (a.name < b.name) return -1
+    else return 0
+})
 
-            return state // need to fix
+            return action.payload === "up" ? newState : newState.reverse()  // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(a => a.age >= 18) // need to fix
         }
         default:
             return state
