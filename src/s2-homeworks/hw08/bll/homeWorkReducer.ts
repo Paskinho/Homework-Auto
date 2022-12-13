46962c3281fb12c323f3472f152eb3c0032bdcf5
Paskinho <v.paskanov@gmail.com>
2022-12-13 12:29:34 +0300

    some refactoring

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,7 +4,7 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
 const newState = [...state].sort((a,b)=> {
@@ -17,7 +17,7 @@ const newState = [...state].sort((a,b)=> {
         }
         case 'check': {
 
-            return state.filter(a => a.age >= 18) // need to fix
+            return state.filter((a:any) => a.age >= 18) // need to fix
         }
         default:
             return state
