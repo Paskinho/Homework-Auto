db782e450acaa7491cd0ace229db6f4eca78375c
Paskinho <v.paskanov@gmail.com>
2023-01-09 19:12:48 +0300

    HW9 completed 1-3 (code)

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,12 +33,22 @@ function Clock() {
         setShow(false)
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    let hours=date.getHours()
+    let mins=date.getMinutes()
+    let secs=date.getSeconds()
+
+    const stringTime = `${hours<10?"0"+hours:hours}:${mins<10?"0"+mins:mins}:${secs<10?"0"+secs:secs}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+
+    let formatter1=new Intl.DateTimeFormat('ru')
+    const stringDate = formatter1.format(date) || <br/>// день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+
+    let formatter2=new Intl.DateTimeFormat('eng',{weekday:"long"})
+    const stringDay = formatter2.format(date) || <br/> // пишут студенты
+
+    let formatter3=new Intl.DateTimeFormat('eng',{month: "long"})
+    const stringMonth = formatter3.format(date) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
