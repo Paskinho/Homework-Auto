5cbb2270c0870ab40f6043d2bb2372ee427a7e8e
Paskinho <v.paskanov@gmail.com>
2023-01-29 16:08:24 +0300

    HW12. 3 дописать тип и логику функции change

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -22,9 +22,10 @@ const themes = [
 const HW12 = () => {
     // взять ид темы из редакса
     const themeId = useSelector<AppStoreType,number>(state => state.theme.themeId)
+const dispatch = useDispatch()
 
-    const change = (id: number) => { // дописать функцию
-
+    const change = (id: any) => { // дописать функцию
+     dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
