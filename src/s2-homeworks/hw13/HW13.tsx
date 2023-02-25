252269c4943cbb717c1a4103042067aa47bd48b0
Paskinho <v.paskanov@gmail.com>
2023-02-25 13:34:55 +0300

    HW 13 * 1 - correct .then

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -37,7 +37,7 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 setText(res.data.errorText)
-                setInfo('...loading')
+                setInfo(res.data.info)
                 // дописать
 
             })
