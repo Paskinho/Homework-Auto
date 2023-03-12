66953401b063f92325123509a3d1c923b17369b3
Paskinho <v.paskanov@gmail.com>
2023-03-12 15:49:57 +0300

    HW14.* 2 - дописать функцию sendQuery в HW14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -6,8 +6,8 @@ import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedI
 import {useSearchParams} from 'react-router-dom'
 
 /*
-* 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
-* 2 - дописать функцию sendQuery в HW14
+* 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput +
+* 2 - дописать функцию sendQuery в HW14 +
 * 3 - дописать функцию onChangeText в HW14
 * 4 - сделать стили в соответствии с дизайном
 * 5 - добавить HW14 в HW5/pages/JuniorPlus
